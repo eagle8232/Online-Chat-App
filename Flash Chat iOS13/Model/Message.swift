35d00e7fa5ad0f6398535be9baed259903cafb00
Vusal <89917025+eagle8232@users.noreply.github.com>
2023-02-06 11:19:55 +0400

    Added Flash Chat to repository

---
+++ Flash Chat iOS13/Model/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Vusal Nuriyev on 9/20/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String 
+}
